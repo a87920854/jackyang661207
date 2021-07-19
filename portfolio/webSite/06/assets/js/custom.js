d4b7a1d60e013a63c4d1a322a49991ab10e435ad
a87920854 <51509697+a87920854@users.noreply.github.com>
2021-07-19 11:49:51 +0800

    modify js

--- portfolio/webSite/06/assets/js/custom.js
+++ portfolio/webSite/06/assets/js/custom.js
@@ -4,7 +4,7 @@
 
 	// Window Resize Mobile Menu Fix
 	mobileNav();
-
+	lightbox();
 
 	// Menu Dropdown Toggle
 	if($('.menu-trigger').length){
@@ -64,5 +64,80 @@
 		});
 	}
 
+	// lightbox
+	function lightbox() {
+		var _el = jQuery(".lightbox");
+
+		if(_el.length > 0) {
+
+			
+				if(typeof(jQuery.magnificPopup) == "undefined") {
+					return false;
+				}
+
+				jQuery.extend(true, jQuery.magnificPopup.defaults, {
+					tClose: 		'Close',
+					tLoading: 		'Loading...',
+
+					gallery: {
+						tPrev: 		'Previous',
+						tNext: 		'Next',
+						tCounter: 	'%curr% / %total%'
+					},
+
+					image: 	{ 
+						tError: 	'Image not loaded!' 
+					},
+
+					ajax: 	{ 
+						tError: 	'Content not loaded!' 
+					}
+				});
+
+				_el.each(function() {
+
+					var _t 			= jQuery(this),
+						options 	= _t.attr('data-plugin-options'),
+						config		= {},
+						defaults 	= {
+							type: 				'image',
+							fixedContentPos: 	false,
+							fixedBgPos: 		false,
+							mainClass: 			'mfp-no-margins mfp-with-zoom',
+							closeOnContentClick: true,
+							closeOnBgClick: 	true,
+							image: {
+								verticalFit: 	true
+							},
+
+							zoom: {
+								enabled: 		false,
+								duration: 		300
+							},
+
+							gallery: {
+								enabled: false,
+								navigateByImgClick: true,
+								preload: 			[0,1],
+								arrowMarkup: 		'<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
+								tPrev: 				'Previous',
+								tNext: 				'Next',
+								tCounter: 			'<span class="mfp-counter">%curr% / %total%</span>'
+							},
+						};
+
+					if(_t.data("plugin-options")) {
+						config = jQuery.extend({}, defaults, options, _t.data("plugin-options"));
+					}
+
+					jQuery(this).magnificPopup(config);
+
+				});
+
+			
+
+		}
+
+	}
 
 })(window.jQuery);
